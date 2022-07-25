2e56c7feb0930c853e7ab9fc0582bc1611b0a55c
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-25 16:50:52 +0800

    Update discord bot.py

--- discord bot.py
+++ discord bot.py
@@ -12,7 +12,7 @@ async def on_ready():
     
 @bot.command()
 async def ping ( ctx ):
-    await ctx.send ( f'{bot.latency*1000}(ms)' )
+    await ctx.send ( f'{round(bot.latency*1000)}(ms)' )
 
 
 bot.run ( jdata[ 'TOKEN' ] )
