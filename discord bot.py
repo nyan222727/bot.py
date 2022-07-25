525cf4a52dea681c25e14834e7f6018be5958e9a
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-25 16:24:52 +0800

    Update discord bot.py

--- discord bot.py
+++ discord bot.py
@@ -4,11 +4,15 @@ import json
 with open ( 'setting.json', 'r', encoding  = 'utf8' ) as jfile:
     jdata = json.load(jfile)
 
-client = discord.Client()
-
 bot = commands.Bot( command_prefix = '[' )
 
 @bot.event
 async def on_ready():
     print (">>bot is online<<")
+    
+@bot.command()
+async def ping ( ctx ):
+    await ctx.send ( bot.latency )
 bot.run ( jdata[ 'TOKEN' ] )
+    
+
