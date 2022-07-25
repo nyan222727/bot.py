bd78e117cf4d6458a5e36fc738a32c959fd2ea05
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-25 15:12:53 +0800

    4

--- discord bot.py
+++ discord bot.py
@@ -1,9 +1,14 @@
 import discord
 from discord.ext import commands
+import json
+with open ( 'setting.json', 'r', encoding  = 'utf8' ) as jfile:
+    jdata = json.load(jfile)
+
+client = discord.Client()
 
 bot = commands.Bot( command_prefix = '[' )
 
 @bot.event
 async def on_ready():
     print (">>bot is online<<")
-bot.run ("OTk3MzE1NDA0MzM2NzI2MDY3.Gs51pt.w3PC_cItXHmCU5VdLbkRTs-9Sfuz6xUpvPm82g")
+bot.run ( jdata[ 'TOKEN' ] )
