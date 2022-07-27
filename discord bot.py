3478996a529c56e6250cbd0330c1985743d69523
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-27 14:12:18 +0800

    a

--- discord bot.py
+++ discord bot.py
@@ -11,6 +11,21 @@ bot = commands.Bot( command_prefix = '[' )
 async def on_ready():
     print (">>bot is online<<")
 
+@bot.command()
+async def load (ctx,extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send (f'Loaded {extension} done')
+    
+@bot.command()
+async def unload (ctx,extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send (f'UN-Loaded {extension} done')
+
+@bot.command()
+async def reload (ctx,extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send (f'RE-Loaded {extension} done')
+
 for Filename in os.listdir('./cmds'):
     if Filename.endswith ('.py'):
         bot.load_extension(F'cmds.{Filename[:-3]}')
