d8c6ebdb8ae58dea5f9236466e401d9c190aa91b
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-27 23:37:51 +0800

    121

--- cmds/react.py
+++ cmds/react.py
@@ -1,11 +1,17 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
+import json
+with open ( 'setting.json', 'r', encoding  = 'utf8' ) as jfile:
+    jdata = json.load(jfile)
 
-class  React(Cog_Extension):
+class React(Cog_Extension):
     @commands.command()
     async def abc(self,ctx):
         await ctx.send('123')
 
+    @commands.command()
+    async def 圖片(self, ctx):
+        await ctx.send(jdata['worship'])
 def setup(bot):
     bot.add_cog (React(bot))
