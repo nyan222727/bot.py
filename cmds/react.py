7c9edd56e9197f9f62047545d1ed5cafa293f36c
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-27 14:36:44 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -4,8 +4,8 @@ from core.classes import Cog_Extension
 
 class  React(Cog_Extension):
     @commands.command()
-    async def hi(self,ctx):
-        await ctx.send('ABCD')
+    async def fuck(self,ctx):
+        await ctx.send('好色喔')
 
 def setup(bot):
     bot.add_cog (React(bot))
