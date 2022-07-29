31feaf01a173dafe22efaf0bcfc2a59356d7a789
01006 <109795457+nyan222727@users.noreply.github.com>
2022-07-29 22:24:10 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -4,9 +4,14 @@ from core.classes import Cog_Extension
 
 class Main(Cog_Extension):
     @commands.command()
-    async def ping ( self,ctx ):
-        await ctx.send ( f'{round(self.bot.latency*1000)}(ms)' )
-    
+    async def ping (self, ctx):
+        await ctx.send (f'{round(self.bot.latency*1000)}(ms)')
+        
+    @commands.command()
+    async def sayd (self, ctx, msg):
+        await ctx.message.delete(msg)
+
 def setup(bot):
     bot.add_cog(Main(bot))
+
 
